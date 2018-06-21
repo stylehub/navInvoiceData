a7f441cb589b585c04a22cc457330e79839cdee6
SZIGETVÁRI Áron <aron.szigetvari@dream.hu>
2018-06-21 17:54:23 +0200

    Adding annotation to Invoice.php (somehow Invoice.yml is not working)

--- src/Invoice.php
+++ src/Invoice.php
@@ -2,8 +2,12 @@
 
 namespace Dream\NavInvoiceData;
 
+use JMS\Serializer\Annotation as Serializer;
+
 /**
  * Class representing Invoice
+ *
+ * @Serializer\XmlRoot(name="Invoice", namespace="http://schemas.nav.gov.hu/OSA/1.0/data")
  */
 class Invoice extends InvoiceType
 {
